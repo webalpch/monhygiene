bbcd640ffdf5adc2926ba8d692c760f58555b062
webalpch <webalpch@gmail.com>
2025-08-25 23:11:37 +0200

    descirption

--- src/data/services.ts
+++ src/data/services.ts
@@ -12,7 +12,7 @@ export const SERVICES: Service[] = [
     id: 'nettoyage-canape',
     name: 'Nettoyage canapé',
     icon: 'Armchair',
-    description: 'Nettoyage professionnel de canapé à domicile avec tarifs selon nombre de places'
+    description: 'Nettoyage de canapé à domicile avec tarifs selon nombre de places'
   },
   {
     id: 'nettoyage-matelas',
@@ -24,25 +24,25 @@ export const SERVICES: Service[] = [
     id: 'shampooinage-sieges',
     name: 'Shampooinage sièges',
     icon: 'Armchair',
-    description: 'Shampooinage professionnel des sièges de véhicule'
+    description: 'Shampooinage des sièges de véhicule'
   },
   {
     id: 'nettoyage-moquette-tapis',
     name: 'Nettoyage moquettes/tapis',
     icon: 'Layers',
-    description: 'Nettoyage professionnel de moquettes et tapis sur devis'
+    description: 'Nettoyage de moquettes et tapis sur devis'
   },
   {
     id: 'nettoyage-vitres',
     name: 'Nettoyage vitres',
     icon: 'Droplets',
-    description: 'Lavage professionnel de vitres intérieur et extérieur'
+    description: 'Lavage de vitres intérieur et extérieur'
   },
   {
     id: 'nettoyage-billard',
     name: 'Nettoyage tapis billard',
     icon: 'CircleDot',
-    description: 'Nettoyage spécialisé du tapis de votre table de billard'
+    description: 'Nettoyage du tapis de votre table de billard'
   },
   {
     id: 'nettoyage-terrasse',
